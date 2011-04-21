d5857f48e8f76b4a2a9c209facc9f7770473db2e
nickklosterman <nick_klosterman@yahoo.com>
2011-04-21 09:17:12 -0400

    plans for dealscraper3.sh

--- DealScraper3.sh
+++ DealScraper3.sh
@@ -2,6 +2,67 @@
 
 #This program scrapes the four deal websites and displays the current deal
 
+function GetPageReturnFile()
+{
+}
+
+function GivePageReturnText()
+{}
+
+function GivePageReturnImage()
+{}
+
+function GivePageReturnTotalQuantity()
+{
+TotalQuantity=`grep total_qty_bar.set_data( ${1} | sed 's/.*,//' | sed 's/).*//' `
+echo ${TotalQuantity}
+}
+
+function GivePageReturnQuantityRemaining()
+{
+RemainingQuantity=`grep total_qty_bar.set_data( ${1} | sed 's/.*(//' | sed 's/,.*//' `
+echo ${RemainingQuantity}
+}
+
+function GivePageReturnQuantityRemainingOfTotalQuantity()
+{
+QuantityRemainingOfTotalQuantity=`grep total_qty_bar.set_data( ${1} | sed 's/.*(//' | sed 's/).*/ left/' | sed 's/,/ of /'  `
+echo ${QuantityRemainingOfTotalQuantity}
+}
+
+function GivePageReturnTimeRemainingOfTotalTime()
+{
+TimeRemainingOfTotalTime=`grep setupWMTimerBar ${1} | sed 's/.*(//' | sed 's/).*/ total/' | sed 's/,/ seconds left of /'  `
+echo ${QuantityRemainingOfTotalQuantity}
+}
+
+function GivePageReturnDurationOfDealInMinutes()
+{
+DurationOfDeal=` grep "Time Remaining:" ${1} -A 4 | grep bar_full | sed 's/<[^>]*>//g' `
+echo ${DurationOfDeal}
+}
+
+function GiveSecondsReturnMinutesAndSeconds()
+{
+    let "minutes = ${1} % 60"
+    let "seconds = ${1} - (60 * ${minutes} )"
+    if [ ${seconds} -lt 10 ]
+    then 
+	echo ${minutes}:0${seconds}
+    elif
+	echo ${minutes}:${seconds} #FML the seconds need to be padded with zeros
+    fi
+}
+
+
+function GivePageReturnTimeRemainingInSeconds()
+{
+TimeRemainingInSeconds=`grep setupWMTimerBar ${1} | sed 's/.*(//' | sed 's/,.*//'   `
+}
+
+function GiveTimeReturnDealEndTime()
+{}
+
 function GetPageReturnText()
 {
 #this function gets a webpage and echoes the name of the file that holds the desired text
@@ -51,7 +112,7 @@ while [ 1 ] ; do
 	date
 	notify-send  "$WhiskeyMilitia" -i ${WhiskeyMilitiaImage} -t 3
     fi
-
+WILL NEED TO PERFORM CHECK TO SEE IF THE OUTPUT ISNT EMPTY DUE TO NOT REPORTING TIME LEFT ON ITEM -> STATE NO TIME LIMIT GIVEN
     Bonktown=$(GetPageReturnText http://www.bonktown.com )
     if [ "${Bonktown}" != "${BonktownTemp}" ]
     then
@@ -75,5 +136,7 @@ while [ 1 ] ; do
     BonktownTemp=`echo ${Bonktown}`
     ChainloveTemp=`echo ${Chainlove}`
    
+need to almost thread this so that each deal is being updated at just the right time.
+Or just update all whenever the next deal goes up. Then find the new deal with the shortest time left and update again. And reevaluate again when that deal is up.
     sleep 5m
 done
