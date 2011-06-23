6a57912300b37e6534d10078a0d4f5c411ad391f
nickklosterman <nick_klosterman@yahoo.com>
2011-06-23 17:40:31 -0400

    added new php forms to delete entries

---
+++ DealScrapers/BackcountryWebsites/querytodelete.php
@@ -0,0 +1,39 @@
+<?php
+$con = mysql_connect("localhost","BCA","backcountryalerts");
+if (!$con)
+{
+die('Could not connect:'.mysql_error());
+}
+
+mysql_select_db("djinnius_BackCountryAlerts",$con);
+
+$sql="SELECT * FROM SearchTermsAndContactAddress WHERE contactaddress = ('$_POST[SearchAddress]')";
+
+if (!mysql_query($sql,$con))
+{
+die('Error:'.mysql_error());
+}
+else
+{
+$result=mysql_query($query);
+}
+//creating the table /w headers
+echo "<html><body>";
+echo "<table><tr><td>ID</td><td>Search Term</td><td>Contact Address</td></tr>";
+
+//row for each record
+while ($row = mysql_fetch_array($rs)) {
+echo"<tr><td>" . $row['prim_key'] . "</td><td>" . $row['searchterms'] . "</td><td>" . $row['contactaddress'] . "</td></tr>";
+}
+
+echo "</table>";
+echo "</body></html>";
+
+
+//free memory
+mysql_free_result($result);
+
+//close the db
+
+mysql_close($con)
+?>
