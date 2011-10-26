8cca96ef804dcaeba25391e4d46ee09f69227b68
Nicolae Ceausescu <nicolae@nicolae-desktop.(none)>
2011-10-26 01:00:13 -0400

    made IBD100stst.sh a lot better, grabs quotes from db and compares to most recent quote

--- IBD100Stats.sh
+++ IBD100Stats.sh
@@ -19,7 +19,7 @@ then
     echo "Files from TargetPath that aren't in ThumbnailPath will be deleted."
     echo "scriptname.sh TargetPath ThumbnailPath"
 else
-    mysql --skip-column-names -s --database StockMarketData -e "select distinct symbol from IBD100 order by symbol asc" -p > /tmp/IBD100stocks.txt
+    mysql --skip-column-names -s --database StockMarketData -e "select distinct symbol from IBD100 order by symbol asc" -pceausescu > /tmp/IBD100stocks.txt
 stocklist=$(cat /tmp/IBD100stocks.txt | tr "\n" "+" )
 	wget "http://download.finance.yahoo.com/d/quotes.csv?s=${stocklist}&f=sl1&e=csv" -O /tmp/quotes.csv -nv
 
@@ -29,12 +29,25 @@ while read Line
 do
 #echo ${Line}
 symbol=$( echo ${Line} | cut -d "," -f 1  )
-mostrecentclosingprice=$( echo ${Line} | cut -d "," -f 2  )
+mostrecentclosingprice=$( echo ${Line} | cut -d "," -f 2  | sed 's/.$//' )
 #min,max()
+
 query="select avg(ClosingPrice) from IBD100 where Symbol=${symbol}";
+query_result=$(  mysql --skip-column-names -s --database StockMarketData -e "${query}" -pceausescu ) #> /tmp/QueryResult
 #--> either need to execute 1 query for each symbol or need another db that holds only 1price for each symbol and then just dump the data from that table. maybe do both and see what time diff is btw methods.
-echo ${symbol} ${mostrecentclosingprice}
-echo ${query}
+if [ 1 -eq 0 ]
+then
+if [ ${mostrecentclosingprice} -gt ${query_result} ]
+then 
+echo ${symbol} ${mostrecentclosingprice} " Gain!! " ${query_result}
+else
+echo ${symbol} ${mostrecentclosingprice} " LOSS!! " ${query_result}
+#echo ${query} ${query_result}
+fi
+fi #end if 1 eq 0
+gain_or_loss=$( echo "scale=2; $mostrecentclosingprice/$query_result" | bc )
+echo ${symbol} ${mostrecentclosingprice}  ${query_result} ${gain_or_loss}
+
 done < /tmp/quotes.csv
 
 #http://www.gummy-stuff.org/Yahoo-data.htm or put in spreadsheet and do it that way
