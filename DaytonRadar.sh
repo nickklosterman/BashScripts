893ac1d47ed13f83edd1d1a03ba411ccc37b139b
Nicolae Ceausescu <nicolae@nicolae-desktop.(none)>
2012-03-15 22:56:55 -0400

    created DaytonRadar.sh to get weather radar image. need to grab text weather forecast.

---
+++ DaytonRadar.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+feh http://radar.weather.gov/lite/N0R/ILN_0.png http://www.goes.noaa.gov/GIFS/ECI8.JPG http://www.goes.noaa.gov/GIFS/ECI7.JPG http://www.goes.noaa.gov/GIFS/ECI6.JPG http://www.goes.noaa.gov/GIFS/ECI5.JPG
+
