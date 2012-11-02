104cd66e6936e8962e720914e5e1319b79a73f72
nickklosterman <nick_klosterman@yahoo.com>
2012-11-02 17:47:03 -0400

    don't know

--- BlogScripts/TumblrRippr.sh
+++ BlogScripts/TumblrRippr.sh
@@ -2,18 +2,18 @@
 
 function getInstagramImages()
 {
-tmpinstagram=$1
-tempinstagrampage=/tmp/tempinstagrampage
+    tmpinstagram=$1
+    tempinstagrampage=/tmp/tempinstagrampage
 
-while read LINE
-do 
-echo $LINE
-wget $LINE -O $tempinstagrampage
-url=$(grep class=\"photo\" $tempinstagrampage | sed 's/.*src="//;s/" alt.*//')
-echo $url
-wget $url
+    while read LINE
+    do 
+	echo $LINE
+	wget $LINE -O $tempinstagrampage
+	url=$(grep class=\"photo\" $tempinstagrampage | sed 's/.*src="//;s/" alt.*//')
+	echo $url
+	wget $url
 
-done < $tmpinstagram
+    done < $tmpinstagram
 
 }
 
@@ -26,40 +26,55 @@ then
 else
     tmptumblr=/tmp/tumblr
     tmpinstagram=/tmp/instagram
+    tmpfilelist=filelist.txt
     url=$1
 #obtain the initial webpage 
     wget "$url" -O index.html -U Firefox
     counter=1
 
-    FileWebAddress=$( grep ">Older<" index.html )
+#	FileWebAddress=$( grep ">Older<" index.html )
+#	FileWebAddress=$( grep "Older" index.html )
+    FileWebAddress=$(grep "BEGIN POSTS" $counter.html -A 2 | grep "END POSTS" ) #this will return nothing until we go beyond the number of pages that there are postings on. 
     echo "++++++--"${FileWebAddress}"--++++++"
 
-    while [[ "$FileWebAddress" != "" ]]
+#    while [[ "$FileWebAddress" != "" ]] # use when using the "Older" paradigm
+    while [[ "$FileWebAddress" == "" ]]
     do
 #get subs files
 
-	wget ${url}/page/${counter} -O ${counter}.html 
-	FileWebAddress=$( grep ">Older<" $counter.html )
-	let 'counter+=10'
+	wget ${url}/page/${counter} -O ${counter}.html   #aagggh damnit, if I have 
+#OB	FileWebAddress=$( grep ">Older<" $counter.html )
+	FileWebAddress=$( grep "Older" $counter.html )  #this method isn't bullet proof. need a way to parse posts. if it is an empty page (a page beyond the true number of posts) then it will have <!-- BEGIN POSTS --> and <!-- END POSTS --> on lines with a blank line in between. 
+	FileWebAddress=$(grep "BEGIN POSTS" $counter.html -A 2 | grep "END POSTS" ) #this will return nothing until we go beyond the number of pages that there are postings on. 
+	let 'counter+=1' #for testing set this to a higher number to increment
     done
 
 #have to specify the files this way since can't use regexp . For why check out : man bash and skip down to Pathname Expansion
 #this will give an error if there are not 1,2, or 3 digit files.
-cat index.html [0-9].html [0-9][0-9].html [0-9][0-9][0-9].html > BLOG.html
+    cat index.html [0-9].html [0-9][0-9].html [0-9][0-9][0-9].html > BLOG.html
 
-grep instagr.am/ BLOG.html | sed 's/.*href="//;s/">.*//'  > $tmpinstagram
+    grep instagr.am/ BLOG.html | sed 's/.*href="//;s/">.*//'  > $tmpinstagram
 #need to grep the individual instagram pages and look for -->grep class=\"photo\" instagrampage | sed 's/.*src=//;s/" alt.*/"/'
 
-getInstagramImages $tmpinstagram
+#  getInstagramImages $tmpinstagram
 
-if [[ 1 -eq 0 ]]
-then
-#grep .media.tumblr BLOG.html | grep src | sed 's/.*src=/wget /;s/" alt.*/"/' > $tmptumblr
+    if [[ 1 -eq 1 ]]
+    then
+#grep .media.tumblr BLOG.html | grep src | sed 's/.*src=/wget /;s/" alt.*/"/' > $tmptumblr  #this most likely just gets the preview images and not the full size images.
 #bash $tmptumblr
 
-grep .media.tumblr BLOG.html | grep src | sed 's/.*src="//;s/" alt.*//' > $tmptumblr
-wget -i $tmptumblr -nc
-fi
+
+
+	if [ -e $tmpfilelist ]
+	then
+	    echo "Overwriting $tmpfilelist"
+	fi
+	grep .media.tumblr BLOG.html | grep lightbox | sed 's/.*href="//;s/">.*//' > $tmpfilelist #get full size images
+#	grep '<a id="post_' BLOG.html | sed 's/.*href="//;s/" >//' > $tmpfilelist
+#	wget -i $tmpfilelist
+# cd into an images directory or do some file cleanup before getting here so the html is separate from the images use pushd popd 
+#wget -i $tmptumblr -nc
+    fi
 
 
 fi #end catching of num expected args
