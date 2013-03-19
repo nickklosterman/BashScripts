fc96bd06c626c899c51365b60a511c1ae5ba0305
nickklosterman <nick_klosterman@yahoo.com>
2013-03-19 17:09:17 -0400

    better grabbing of regional weather text

--- Weather/HamiltonCountyOhioWeatherText.sh
+++ Weather/HamiltonCountyOhioWeatherText.sh
@@ -5,4 +5,7 @@ wget -q "http://www.nws.noaa.gov/view/prodsByState.php?state=OH&prodtype=hourly"
 echo "--------============----------"
 
 #this is a little overkill since it gets info for the whole state and that ends up being a ~1MB file. The connection is BW limited I bet at ~30kb/s so it takes about 30s to get the file. <---this was MUCH faster when I plugged the ethernet cord straight into the pc instead of going from wireless router/mdem to my netgear router. #nice full text forecast for entire state.
-wget -q "http://www.nws.noaa.gov/view/prodsByState.php?state=OH&prodtype=zone" -O /tmp/fc.html && grep -A 28 -m 1 OHZ077 /tmp/fc.html #without the -m 1 we get the latest 3 weather reports.
+#wget -q "http://www.nws.noaa.gov/view/prodsByState.php?state=OH&prodtype=zone" -O /tmp/fc.html && grep -A 28 -m 1 OHZ077 /tmp/fc.html #without the -m 1 we get the latest 3 weather reports.
+
+
+wget -q "http://www.nws.noaa.gov/view/prodsByState.php?state=OH&prodtype=zone" -O /tmp/fc.html && awk '/OHZ077/{found=1} found{print; if(/\$\$/) exit}' /tmp/fc.html 
