e4d1402f6574a810a1a3fc9b0e9633d0afac78c5
nickklosterman <nick_klosterman@yahoo.com>
2014-02-07 04:08:47 +0000

    eastbay compiles list and outputs list instead of simple printing

--- DealScrapers/eastbayScraper.py
+++ DealScrapers/eastbayScraper.py
@@ -1,9 +1,37 @@
 import urllib.request
 import json
-#desiredStyles=[ 1051629, 10516415,10516735,1051697]
+
+import getopt
+import sys
+
 desiredStyles=[ '1051629', '10516415','10516735','1051697']
+desiredStyles=[]
 priceThreshold=110
 shoeUrl="http://www.eastbay.com/product/model:188492/sku:1051624/mizuno-wave-creation-14-mens/black/orange/?cm=GLOBAL%20SEARCH%3A%20KEYWORD%20SEARCH"
+
+
+try:
+    options, remainder = getopt.gnu_getopt(sys.argv[1:], 'p:u:s:', ['style=',
+                                                                    'price-threshold=',
+                                                                    'url='
+                                                                ])
+except getopt.GetoptError as err:
+    # print help information and exit:
+    print( str(err)) # will print something like "option -a not recognized"  
+    #usage()
+    sys.exit(2)
+
+for opt, arg in options:
+    if opt in ('-s', '--style'): #check for stocks where loss is > 8 %
+        desiredStyles.append(arg)
+    elif opt in ('-u', '--url'):
+        shoeUrl=arg
+    elif opt in ('-p', '--price-threshold'):
+        priceThreshold=float(arg)
+    else:
+        assert False, "unhandled option"
+
+#print(desiredStyles)
 
 socket = urllib.request.urlopen(shoeUrl) #http://www.eastbay.com/product/model:199132/sku:99424008?green=9CB0EEEA-AC14-56AB-B303-ED04C363AFC4&cm=CrossSellMB#sku=99424010&size=09.5")
 htmlSource=socket.read() #.decode('utf-8') #reads contents into variable
@@ -20,7 +48,7 @@ for item in splitHtml:
 #print(myList[0])
 
 #UnicodeDecodeError: 'utf-8' codec can't decode byte 0xba in position
-
+outputList = []
 jsonString=json.loads(myList[0])
 for majorkey,subdict in jsonString.items():
     #print(majorkey,subdict)
@@ -32,10 +60,13 @@ for majorkey,subdict in jsonString.items():
         for item in shoeSizeList:
             if item[0] == ' 09.5':
                 if float(item[2])<priceThreshold:
-                    print("Product #: %s, Shoe Size: %s, Sale Price: $%s, Style: %s, Fit/Width: %s" % (majorkey,item[0],item[2],subdict[15],subdict[16]))
-    
-#    for subkey,subdict2 in subdict:
-#        print(subkey,subdict2)
-#    for subkey,value in subdict:
-#        print(subkey,value)
-#for item in data_string[""]:
+                    #print("Product #: %s, Shoe Size: %s, Sale Price: $%s, Style: %s, Fit/Width: %s" % (majorkey,item[0],item[2],subdict[15],subdict[16]))
+                    outputList.append(("Product #: %s, Shoe Size: %s, Sale Price: $%s, Style: %s, Fit/Width: %s" % (majorkey,item[0],item[2],subdict[15],subdict[16])))
+
+if (len(outputList)>0):
+    print(shoeUrl)
+    print("") #without these extra newlines its all smashed on one line in the email
+    for item in outputList:
+        print(item)
+        print("")
+            
