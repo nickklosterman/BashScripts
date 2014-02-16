01e852bbe1692218f6facb59a062a2034f0c3fc3
nick klosterman <nick_klosterman@yahoo.com>
2014-02-16 11:10:56 -0500

    added options for youtube to dl metadata

--- Youtube/youtube-dlFrontEnd.sh
+++ Youtube/youtube-dlFrontEnd.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
 
+ # --write-description              write video description to a .description file
+ #    --write-info-json                write video metadata to a .info.json file
+ #    --write-annotations              write video annotations to a .annotation file
+ #    --write-thumbnail 
+
+options="--write-description --write-info-json --write-annotations --write-thumbnail --no-playlist"
 if [ $# -lt 1 ]
 then
-    echo "scriptname.sh directions;"
+    echo "${0} directions;"
 else
     if [ -f "$1" ] #if we specified a file on the command line then read the file and spit it out else parse args as stock tickers
     then
+	echo "Executing on a file"
 	while read LINE
 	do
-	    ticker=$(echo "$LINE" | tr '[a-z]' '[A-Z]' )
-	    QueryForQuote $ticker
+	    echo " operating on ${LINE}"
+	    youtube-dl ${options} "${LINE}"
 	done < "$1"
     else 
 	until [ -z "$1" ] #parse args as stock tickers
 	do
-	    ticker=$(echo "$1" | tr '[a-z]' '[A-Z]' )
-	    QueryForQuote $ticker
-#	    symbol=$1
-#	url="'http://download.finance.yahoo.com/d/quotes.csv?s=${symbol}&f=l1'"
-#echo $url
-#	    quote=$(curl -s "http://download.finance.yahoo.com/d/quotes.csv?s=${symbol}&f=l1" | sed 's/.$//' ) #the trailing sed command strips a ^M that was at the end of each quote price
-#	    if [ $quote == "0.00" ]
-#	    then 
-#		echo "uh oh!"
-#	    fi
-#	    echo ${symbol} ${quote}
+	    #CLI method won't work with URLs grabbed from "lists" as the & specifying the list parameter breaks teh CLI as it is interpreted as a backgrounding action. 
+	    #	    echo "youtube-dl '${1}'"
+	    echo " operating on ${1}"
+	    youtube-dl ${options} "${1}"
 	    shift
 	done 
-	
-	echo "could check if the cli arg is a file and attempt to open otherwise treat as a stock symbol and do a query"
     fi
 fi
+
+
+
+
