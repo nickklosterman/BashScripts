c5ed6b2d571d2e86d0d792caa248eb63d710c802
nick klosterman <nick_klosterman@yahoo.com>
2014-03-20 22:25:58 -0400

    merge LKN, first commit of colorRotator

---
+++ ImageRelatedScripts/ColorRotator.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+until [  -z "${1}" ]
+do
+    filename="${1}"
+    name=${filename%.*}
+    extension=${filename##*.}
+    echo $name; echo $extension
+    start=0
+    while [ $start -lt 380 ]
+    do 
+	#convert ${filename} -modulate 100,100,${start} ${name}_${start}.${extension}
+	#convert ${filename} -modulate 100,100,${start} ${name}_${start}.gif
+	convert ${filename} -resize 50% -modulate 100,100,${start} ${name}_${start}.gif
+	#echo ${start} ${name}_${start}${extension}  
+	#echo $start
+	let start+=20
+    done
+
+    gifsicle --delay 140 --loopcount  ${name}_*.gif > ${name}-anim.gif
+    #gifsicle ${name}_*.gif -d 80 --loopcount > ${name}_anim.gif
+    rm ${name}_*.gif
+    animate ${name}-anim.gif
+    shift 
+done
