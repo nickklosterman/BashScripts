025246cd625c4f30f856fba04bb9fc901b2cbd8a
nickklosterman <nick_klosterman@yahoo.com>
2014-04-15 18:41:13 -0400

    added checking of filename collisions of extracted images

--- comicCoverScraper.sh
+++ comicCoverScraper.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 function determineArchiveType()
 {
     flag=2 #0 for zip, 1 for rar, 2 for unknown
@@ -24,16 +23,40 @@ function addArchiveLabelToImage() {
     convert "${1}" -background White -fill Black -font Courier -pointsize 24 label:"${2}" -gravity South -append _comiccovers"${1}"
 }
 
+function checkFilenameDoesntExist() {
+
+imageToExtract="${1}"
+if [ -e "${imageToExtract}" ]
+then
+    echo "uh oh, file all ready exists"
+    read key
+fi
+counter=1
+temp="${imageToExtract}"
+while [ -e "${imageToExtract}" ]
+do 
+extension=${imageToExtract##*.}
+filename=${imageToExtract%.*}
+number=$( printf "%03d" $counter )
+imageToExtract="${filename}${number}.${extension}"
+let 'counter+=1'
+done
+echo "${imageToExtract}" #for a better way to return values to callers http://www.linuxjournal.com/content/return-values-bash-functions
+}
+
 function extractFirstImageFromArchive() 
 {
+#we need to check if the file we are about to extract to all ready exists and if so to rename it.
     if [ $1 -eq 0 ]
     then #Zip 
 	imageToExtract=$( unzip -Z -1 "${2}" | sed '1!d' )
+#	checkFilenameDoesntExist "${imageToExtract}"  fuck since we 'return' things from this function, the 'return' eats  the dialog....damnit.
 	b=$( unzip "${2}" "${imageToExtract}" )
     fi
     if [ $1 -eq 1 ] 
     then #Rar
 	imageToExtract=$( unrar lb "${2}" | sed '1!d' )
+#	checkFilenameDoesntExist "${imageToExtract}"
 	b=$( unrar e "${2}" "${imageToExtract}" )
     fi
     echo "${imageToExtract}"
@@ -50,18 +73,35 @@ function extractFirstImageFromArchive()
 #     #mv "${imageName}"  comiccovers_"${imageName}" 
 # done
 
-for i in *.cbz
-do 
-    echo "${i}"
-    archiveType=$( determineArchiveType "${i}" )
-    echo $archiveType
-    imageName=$( extractFirstImageFromArchive ${archiveType}  "${i}" )
-    echo ${imageName}
-    addArchiveLabelToImage "${imageName}" "${i}" 
-#    mv "${imageName}"  comiccovers_"${imageName}" 
-done
+if [ 1 -eq 0 ]
+then
+    #for i in `find . -type f -name "*.[Cc][Bb][RrZz]" ` #-print0` #*.cbz
+    for i in *.cbz
+    do 
+	echo "${i}"
+	archiveType=$( determineArchiveType "${i}" )
+	echo $archiveType
+	imageName=$( extractFirstImageFromArchive ${archiveType}  "${i}" )
+	echo ${imageName}
+	addArchiveLabelToImage "${imageName}" "${i}" 
+	#    mv "${imageName}"  comiccovers_"${imageName}" 
+    done
 
-zip ComicCovers _comiccovers*.*
-mv ComicCovers.zip ComicCovers.cbz
-evince ComicCovers.cbz
-# rar wtf rar is only in the AUR
+    zip ComicCovers _comiccovers*.*
+    mv ComicCovers.zip ComicCovers.cbz
+    evince ComicCovers.cbz
+    # rar wtf rar is only in the AUR
+fi
+
+
+#from: http://stackoverflow.com/questions/1116992/capturing-output-of-find-print0-into-a-bash-array
+unset a i
+while IFS= read -r -d $'\0' file; do
+    echo "$file"        # or however you want to process each file
+	archiveType=$( determineArchiveType "${file}" )
+	echo $archiveType
+	imageName=$( extractFirstImageFromArchive ${archiveType}  "${file}" )
+	echo ${imageName}
+	addArchiveLabelToImage "${imageName}" "${file}" 
+
+done < <(find . -type f -name "*.[Cc][Bb][RrZz]" -print0)
