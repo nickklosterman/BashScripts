7967eba12cdbce31f435afa59e45428e0f85f520
nickklosterman <nick_klosterman@yahoo.com>
2014-04-21 13:19:19 -0400

    now saves image, working on looping until nextlink is invalid

--- WebComics/node/webcomicScraper.js
+++ WebComics/node/webcomicScraper.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var fs = require('fs');
 
 urlArray = [
     "http://cucumber.gigidigi.com/cq/page-1/"
@@ -10,6 +11,8 @@ var getNextLink= function(body) {
     var nextLinkElement=$('link[rel=\'next\']');
     var nextLink=nextLinkElement[0].attribs.href;
     console.log(nextLinkElement[0].attribs.href);
+    nextlink = nextLinkElement[0].attribs.href;
+    return nextLinkElement[0].attribs.href;
 };
 
 var getComicImage=function(body){
@@ -30,20 +33,49 @@ var getComicImage=function(body){
     //	console.log(comicImageElement[0].attribs.src);
     var comicImage=comicImageElement[0].attribs.src;
     console.log(comicImage);
-
+//    var filename = sprintf("test%03d.png",) ; //shit didn't realize there is js implementation of sprintf but I don't wanna dl. http://www.diveintojavascript.com/projects/javascript-sprintf :(
+    var fileprefix="test";
+    var filenumber=pad(imagecounter,4);
+    var filesuffix="png";
+    var filename=fileprefix+filenumber+"."+filesuffix;
+    request(comicImage).pipe(fs.createWriteStream(filename)) //from  https://www.npmjs.org/package/request
 };
 
+var pad=function(num, size) { //http://stackoverflow.com/questions/2998784/how-to-output-integers-with-leading-zeros-in-javascript 
+    var s = "0000" + num;
+    return s.substr(s.length-size);
+}
+
 for (var counter = 0; counter < urlArray.length; counter++) {
+    var imagecounter=0;
     var url=urlArray[counter];
+    var nextlink=url;
     console.log('----');
     console.log(url);
     console.log('----');
-    request(url, function(err, resp, body) {
+    // request(url, function(err, resp, body) {
+    //         if (err)
+    // 		throw err;
+    //         $ = cheerio.load(body); //hmm so $ is global??
+    // 	nextlink=getNextLink();
+    // 	getComicImage();
+    // });
+
+//this is a variable scoping problem with nextlink not changing.
+    while (typeof nextlink !=='undefined' && imagecounter<10)
+    {
+	console.log(nextlink);
+	request(nextlink, function(err, resp, body) {
             if (err)
     		throw err;
             $ = cheerio.load(body); //hmm so $ is global??
-	getNextLink();
-	getComicImage();
-    });
-   
+	    console.log(imagecounter);
+	    imagecounter+=1;
+	    nextlink=getNextLink();
+	    console.log(nextlink)
+	    getComicImage();
+	});	
+    }
+    
+    
 }
