ab3f20019c7ba70cefe01611229f27fef0588ee0
nickklosterman <nick_klosterman@yahoo.com>
2014-04-29 11:23:20 -0400

    cleaned up what is displayed

--- DealScrapers/BackcountryWebsites/SimpleXMLScraper.sh
+++ DealScrapers/BackcountryWebsites/SimpleXMLScraper.sh
@@ -6,12 +6,12 @@ wget http://rss.steepandcheap.com/docs/steepcheap/rss.xml -O /tmp/sac.xml -nv
 echo "--**||**--"
 echo "Whiskey Militia"
 echo "--**||**--"
-grep 'title\|:price>' /tmp/wm.xml
+grep 'title\|:price>' /tmp/wm.xml | sed -n -e '1,5p' | sed 's/<title>//;s/<\/title>//;s/<odat:price>//;s/<\/odat:price>//;'
 echo "--**||**--"
 echo "Steep And Cheap"
 echo "--**||**--"
-grep 'title\|:price>' /tmp/sac.xml
+grep 'title\|:price>' /tmp/sac.xml | sed -n -e '2,5p' | sed 's/<title>//;s/<\/title>//;s/<sac:price>//;s/<\/sac:price>//;'
 echo "--**||**--"
 echo "Chain Love"
 echo "--**||**--"
-grep 'title\|:price>' /tmp/cl.xml
+grep 'title\|:price>' /tmp/cl.xml | sed -n -e '1,5p' | sed 's/<title>//;s/<\/title>//;s/<odat:price>//;s/<\/odat:price>//;'
