c4c6b69ec30d6e5970e6f806c19dee706517d188
nickklosterman <nick_klosterman@yahoo.com>
2014-05-14 15:11:57 -0400

    BC node dealscraper now gets latest previous record from mongo on startup to prevent multiple inserts of same item on when app restarted

--- DealScrapers/BackcountryWebsites/node/pageScraper/webRssScraperWebPageData.js
+++ DealScrapers/BackcountryWebsites/node/pageScraper/webRssScraperWebPageData.js
@@ -6,17 +6,46 @@ var SACtimeremaining = 0.001
 , WMtimeremaining = 0.001
 , CLtimeremaining = 0.001;
 
-
-var previous = {SAC: "",
-WM: "",
-CL: ""};
-
 var pageArray = [
     {url:"http://www.steepandcheap.com/current-steal",type:0,site:"SAC",previous:""},
     {url:"http://www.whiskeymilitia.com/",type:1,site:"WM",previous:""},
     {url:"http://www.chainlove.com/",type:1,site:"CL",previous:""}
 ];
 
+
+var getLastEntry=function(Obj){
+    mongoClient.connect('mongodb://127.0.0.1:27017/bc',function(err,db){
+        if (err) {throw err;}
+        //deals will be the name of the table/colleciton
+        var collection  = db.collection('deal');
+
+//	if (typeof collection !== 'undefined')	{console.log(site);}
+//using findOne was causing sort to not work. 
+	collection.find({site:Obj.site}).sort({_id:-1}).limit(1).toArray(function(err,data) {
+            if (err) { throw err;}
+            else {
+                switch(Obj.site){
+                case "WM":
+		    console.log("WM:"+data[0].productTitle);
+		    pageArray[1].previous=data[0];
+		    break;
+                case "CL":
+		    console.log("CL:"+data[0].productTitle);
+		    pageArray[2].previous=data[0];
+                    break;
+                case "SAC":
+		    console.log("SAC:"+data[0].brand.name+' '+data[0].name);
+		    pageArray[0].previous=data[0];
+                    break;
+		default:
+		    console.log("no site match");
+		    break;
+                }
+            }
+            db.close();//node will hang without this
+	});
+    });
+};
 
 var stripData=function(pageObj){
     //find index of our string for our json, find the index of the EOL starting from where we found our first search text. 
@@ -51,9 +80,9 @@ var stripData=function(pageObj){
 	    console.log(parsedDetailsJSON.name+" SACtr:"+SACtimeremaining);
 	    setTimeout(stripData,SACtimeremaining*1000,pageArray[0]);
 	    if (pageObj.previous.odatId != parsedDetailsJSON.odatId) {
+		console.log("Entering SAC data:"+pageObj.previous.odatId+' -> '+parsedDetailsJSON.odatId);
 		pageObj.previous = parsedDetailsJSON;
-console.log("Entering SAC data");
-		enterData(parsedDetailsJSON);
+//		enterData(parsedDetailsJSON);
 	    }
 	    break;
 	case "WM":
@@ -69,11 +98,12 @@ console.log("Entering SAC data");
 	    console.log(parsedDetailsJSON.productTitle+"WMtr:"+WMtimeremaining+' /'+WMduration);
 	    setTimeout(stripData,WMtimeremaining*1000,pageArray[1]);
 
-
 	    if (pageObj.previous.odat_id !== parsedDetailsJSON.odat_id) {
+	    //if (parseInt(pageObj.previous.odat_id,10) !== parseInt(parsedDetailsJSON.odat_id,10)) {
 		pageObj.previous = parsedDetailsJSON;
-		console.log("Entering WM data");
-		enterData(parsedDetailsJSON);
+		console.log("Entering WM data:"+pageObj.previous.odat_id+' -> '+parsedDetailsJSON.odat_id);
+		pageObj.previous = parsedDetailsJSON;
+//		enterData(parsedDetailsJSON);
 	    }
 	    break;
 	case "CL":
@@ -89,9 +119,9 @@ console.log("Entering SAC data");
 	    setTimeout(stripData,CLtimeremaining*1000,pageArray[2]);
 	    parsedDetailsJSON.duration=CLduration;
 	    if (pageObj.previous.odat_id != parsedDetailsJSON.odat_id) {
+		console.log("Entering CL data:"+pageObj.previous.odat_id+' -> '+parsedDetailsJSON.odat_id);
 		pageObj.previous = parsedDetailsJSON;
-		console.log("Entering CL data");
-		enterData(parsedDetailsJSON);
+//		enterData(parsedDetailsJSON);
 	    }
 	    break;
 	default: 
@@ -125,15 +155,21 @@ console.log("Entering SAC data");
     }
 
 if ( 1===1){
-    for (var counter = 0; counter < pageArray.length; counter++) {
-    	var page = pageArray[counter];
-    	stripData(page);
+
+    //get the most recent entries from the database
+
+    for (var cntr = 0; cntr < pageArray.length; cntr++) {
+	getLastEntry(pageArray[cntr]);
     }
 
-// setTimeout(stripData(pageArray[0]),SACtimeremaining*1000);
-// setTimeout(stripData(pageArray[1]),WMtimeremaining*1000);
-// setTimeout(stripData(pageArray[2]),CLtimeremaining*1000);
-
+//Delay the start so that the previous fields are populated.
+    var delayed = function() {
+	for (var counter = 0; counter < pageArray.length; counter++) {
+    	    var page = pageArray[counter];
+    	    stripData(page);
+	}
+    }
+    setTimeout(delayed,500);
 
 } else {
 
