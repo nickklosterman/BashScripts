6f8428eb19afa7b45813c5770583dbcfc47dc251
nickklosterman <nick_klosterman@yahoo.com>
2014-05-16 17:10:54 -0400

    app.js All route works

--- DealScrapers/BackcountryWebsites/node/dataDisplay/app.js
+++ DealScrapers/BackcountryWebsites/node/dataDisplay/app.js
@@ -2,7 +2,7 @@ var express = require('express')
 , mongoClient = require('mongodb').MongoClient;
 
 var app = express();
-var getRecordsFromDB=function(site) {
+function getRecordsFromDB(site,callback) {
     var output;
     mongoClient.connect('mongodb://127.0.0.1:27017/bc',function(err,db){
 	if (err) {throw err;}
@@ -10,7 +10,7 @@ var getRecordsFromDB=function(site) {
 	var collection  = db.collection('deal');
 	output="<ul>";
 	var image;
-console.log(site);
+//console.log(site);
 	collection.find({site:site}).sort({_id:-1}).limit(14).toArray(function(err,data) {
 	    if (err) { throw err;}
 	    else {
@@ -20,53 +20,84 @@ console.log(site);
 		    for (var counter = 0, size=data.length; counter<size;counter++){
 			//for these, the quantity is in the 'variant' data; I think it might also be elsewhere in the html of the page in the javascript
 			if (data[counter].variants) { 
-			    for (var key in data[counter].variants) {console.log(key); break;}
-			    console.log(key);
+			    for (var key in data[counter].variants) { /*console.log(key);*/ break;}
+//			    console.log(key);
 			    if (data[counter].variants[key].images && data[counter].variants[key].images.mediumImage !== null){
 				image =  data[counter].variants[key].images.mediumImage;
 			    } else if (data[counter].variants[key].images && data[counter].variants[key].images.smallImage !== null){
 				image = data[counter].variants[key].images.smallImage;
 			    } else { image = ":(";}
 			}
-			console.log(data[counter].productTitle+' $'+data[counter].price);
+//			console.log(data[counter].productTitle+' $'+data[counter].price);
+			var link="";
+			if (counter===0 ) {
+			    switch (site){
+			    case "WM":
+				link="http://www.whiskeymilitia.com";
+				break;
+
+			    case "CL":
+				link="http://www.chainlove.com";
+				break;
+			    }
+			    output+='<li> <a href="'+link+'"><img  src="'+image+'"> </a>'+data[counter].productTitle+' $'+data[counter].price+'</li>';
+			} else {
 			output+='<li> <img src="'+image+'">'+data[counter].productTitle+' $'+data[counter].price+'</li>';
+			}
 		    }
 		    break;
 		case "SAC":
 		    for (var counter = 0, size=data.length; counter<size;counter++){
-			console.log(data[counter].brand.name+data[counter].name+' $'+data[counter].price);
+//			console.log(data[counter].brand.name+data[counter].name+' $'+data[counter].price);
+			
 			if (data[counter].defaultImage && data[counter].defaultImage.url.medium) {
 			    image = data[counter].defaultImage.url.medium;
 			} else if (data[counter].defaultImage && data[counter].defaultImage.url.tiny) {
 			    image = data[counter].defaultImage.url.url.tiny;
 			}
-			output+='<li> <img height="100" width="100" src="'+image+'">'+data[counter].brand.name+' '+data[counter].name+' $'+data[counter].price+' duration:'+data[counter].duration+' quantity:'+data[counter].qtyInitial+'</li>';
+			if (counter===0) {
+			    output+='<li> <a href="http://www.steepandcheap.com/current-steal"> <img height="100" width="100" src="'+image+'"> </a>'+data[counter].brand.name+' '+data[counter].name+' $'+data[counter].price+' duration:'+data[counter].duration+' quantity:'+data[counter].qtyInitial+'</li>';
+			} else {
+			    output+='<li> <img height="100" width="100" src="'+image+'">'+data[counter].brand.name+' '+data[counter].name+' $'+data[counter].price+' duration:'+data[counter].duration+' quantity:'+data[counter].qtyInitial+'</li>';
+			}
 
 		    }
 		    break;
 		}
 	    }
 	    db.close();//node will hang without this 
+	output+="</ul>";
+//	console.log(output);
+	console.log("finish up getRecordsFromDB");
+	callback( output);
+
 	});
+
     });
-output+="</ul>";
-return output;
+
 };
 
 app.get('/data/',function(req, res) {
-var output="<meta http-equiv='refresh' content='60'>"
-    output+=getRecordsFromDB("SAC");
-    output+="<hr>"
-    output+=getRecordsFromDB("CL");
-    output+="<hr>"
-    output+=getRecordsFromDB("WM");    
-    res.send(output);
+    var output="<meta http-equiv='refresh' content='60'>"
+    getRecordsFromDB("SAC",function(o){output+=o+"<hr>"; sendResponse(); console.log("1");});
+    getRecordsFromDB("CL", function(o){output+=o+"<hr>"; sendResponse(); console.log("2");});
+    getRecordsFromDB("WM", function(o){output+=o;        sendResponse(); console.log("3");});
+    var counter=0;
+    function sendResponse(){
+	console.log("send resp: ctr:"+counter);
+	counter+=1;
+	if (counter===3) {  
+	    res.send(output); // I need this to wait until the output is all returned 
+	}
+    }
+
+  //  console.log("finish up app.get /data/ route");
 //next();
 });
 
 app.get('/data/:site?', function( req, res, next) {
     var site = req.params.site;
-//  res.send('you want data from '+site);
+    //res.send('you want data from '+site);
     var output="";
     mongoClient.connect('mongodb://127.0.0.1:27017/bc',function(err,db){
 	if (err) {throw err;}
@@ -114,13 +145,9 @@ app.get('/data/:site?', function( req, res, next) {
 	    db.close();//node will hang without this 
 	    res.send(output);
 	});
-
-
     });
-
 //    next(); //if I by default just hit next, since I already wrote via res.send, the next res.send will give me the "Error: Can't set headers after they are sent." error
 //next(); //otherwise it won't fall through to next route
-
 });
 
 app.get('/data/*?',function(req, res) {
