5c0666d2140376580c6cce0fa5d948f86e571010
nickklosterman <nick_klosterman@yahoo.com>
2014-10-14 17:05:46 -0400

    url parser has bugs as it is returning false positives

--- MuddleParser.js
+++ MuddleParser.js
@@ -28,7 +28,7 @@ rl.on('line',function(line){
     if (dateMatch !== null) {
 	if (typeof buffer !== 'undefined' 
 	    && buffer !== '') {
-	    console.log('"content":"'+buffer+'"')
+	    console.log('"content":'+JSON.stringify(buffer))
 	    //only output linkBuffer if there is content
 	    if (typeof linkBuffer !== 'undefined' ){
 		//slice off last comma
@@ -43,12 +43,12 @@ rl.on('line',function(line){
     }
     //if the line doesn't contain a date, add content to buffer
     if (dateMatch===null) {
-	var links = extractLinks(line)
+	var links = extractLinks(line.replace(/"/g,'\"'))
 	if ( links !== null ) { 
 	    linkBuffer += links+",\n"
 	}
 	if ( line !== '' ) {
-	    buffer+=line+'\\n'
+	    buffer+=line.replace(/"/g,'\"')+'\\n'
 	}
     }
 })
@@ -59,7 +59,7 @@ rl.on('close',function() {
 
     if (typeof buffer !== 'undefined' 
 	&& buffer !== '') {
-	console.log('"content":"'+buffer+'"')
+	console.log('"content":'+JSON.stringify(buffer))
     }
     //only output linkBuffer if there is content
     if (typeof linkBuffer !== 'undefined' ){
@@ -77,15 +77,34 @@ function extractLinks(line) {
     //http://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-an-url
 //relevant about matching urls: http://stackoverflow.com/questions/37684/how-to-replace-plain-urls-with-links
 //http://stackoverflow.com/questions/26348327/javascript-regex-match-text-after-pattern my question with answer to match the text after the pattern
-    var urlRE= new RegExp("((?:[a-zA-Z0-9]+://)?(?:[a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?(?:[a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(?::[0-9]+)?(?:[^ ])+)(.*)$");
+//    var urlRE= new RegExp("((?:[a-zA-Z0-9]+://)?(?:[a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?(?:[a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(?::[0-9]+)?(?:[^ ])+)(.*)$");
+// var urlRE = new RegExp('^(https?:\\/\\/)?'+ // protocol
+//   '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+//   '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+//   '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+//   '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+//   '(\\#[-a-z\\d_]*)?(.*)$','i');  // -----this one creates a group for each match. we don't want that!
+// urlRE = new RegExp('^(https?:\\/\\/)?'+ // protocol
+//   '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+//   '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+//   '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+//   '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+//   '(\\#[-a-z\\d_]*)?(.*)$','g');  // -----this one creates a group for each match. we don't want that!
+
+//slightly modified RE that matches http at the beg
+//var  urlRE= new RegExp("(([http?]+:\/\/)?(?:[a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?(?:[a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(?::[0-9]+)?(?:[^ ])+)(.*)$"); //is matching traffic.libsyn somehow
+//var  urlRE= new RegExp("^(((http?|ftp|file)+://)?(?:[a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?(?:[a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(?::[0-9]+)?(?:[^ ])+)(.*)$"); //line has to start w url so url can't be later in string :(
+//    var urlRE= new RegExp("((?:[a-zA-Z0-9]{3,5}://)?(?:[a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?(?:[a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(?::[0-9]+)?(?:[^ ])+)(.*)$"); //tried to restrict protocol to 3-5 chars.....but for traffic.libysn there is no protocol! shit
+var  urlRE= new RegExp("(((http?|ftp|file)+://)?(?:[a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?(?:[a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(?::[0-9]+)?(?:[^ ])+)(.*)$"); //line has to start w url so url can't be later in string :(
     var match = urlRE.exec(line);
     if ( match !== null ) {
-	var url=match[1]
-	var description=match[2]
+//	console.log(match)
+	var url=match[1] //match[1]+match[2]+match[3]+match[4]+match[5]+match[6]
+	var description=match[3]//match[2]
 //	console.log("---------:\\nmatch[0]:"+match[0] + "\\nmatch[1]:" + match[1] + "\\nmatch[2]:" + match[2]);
 	if (typeof description !== 'undefined' 
 	    && description !== '' ) {
-	    return '{\n"link":"'+url+'",\n"description":"'+description.trim()+'"\n}'
+	    return '{\n"link":"'+url+'",\n"description":'+JSON.stringify(description.trim())+'\n}'
 	} else {
 	    return '{\n"link":"'+url+'",\n"description":"'+url+'"\n}'
 	}
@@ -95,3 +114,11 @@ function extractLinks(line) {
 
 
 }
+
+//to validate the output json , install jsonlint
+// npm install jsonlint -g 
+// then run 
+// jsonlint myfile.json
+
+//instead of buffer.replace(/"/g,'\"') to escape double quotes, JSON.stringify seemed a much better solution
+//http://stackoverflow.com/questions/2732409/how-can-i-put-double-quotes-inside-a-string-within-an-ajax-json-response-from-ph be careful though using JSON.parse & JSON.stringify
